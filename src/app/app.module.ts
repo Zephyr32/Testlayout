477411f0f6cf084f3b4cf9a39f7064b687da4a0f
Bogdan <35693010+Zephyr32@users.noreply.github.com>
2020-12-05 01:16:27 +0200

    refactoring)
    create components and sort element

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { ServicesComponent } from './services/services.component';
+import { FeaturesComponent } from './features/features.component';
+import { AboutComponent } from './about/about.component';
+import { OurWorkComponent } from './our-work/our-work.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    ServicesComponent,
+    FeaturesComponent,
+    AboutComponent,
+    OurWorkComponent
   ],
   imports: [
     BrowserModule
